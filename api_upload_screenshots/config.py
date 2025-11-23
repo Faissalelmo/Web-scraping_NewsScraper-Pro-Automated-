0d03be46bff2f62379e83a4277484049597b81c4
Faissal Elmokaddem <elmokaddem.faissal.04.12.2002@gmail.com>
2025-11-23 16:49:49 +0100

    Delete api_upload_screenshots directory

--- api_upload_screenshots/config.py
+++
@@ -1,6 +0,0 @@
-API_URL = "http://192.168.3.22/API/RDP/006/001.php"
-FOLDER_PATH = r"storage/test"  
-FOLDER_PATH_PROCESSED = r"storage/data/processed_files"  
-USER_ID = 1
-LOG_FILE = r"logs/upload_log.txt"
-THREADS = 1
